966d8e1c2a0ef6d2cd21b6fc3011e5a7791e7bcb
Brice Cabioch <cabioch.brice@gmail.com>
2023-11-11 13:50:53 +0100

    handle keyboard input and implemented the end game screen in script.js.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+window.onload = function () {
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+
+  startButton.addEventListener("click", function () {
+    startGame();
+    let game;
+  });
+
+  function startGame() {
+    game = new Game();
+    // console.log("start-game");
+    game.start();
+  }
+
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  // Add the handleKeydown function as an event listener for the keydown event
+  window.addEventListener("keydown", handleKeydown);
+
+  restartButton.addEventListener("click", function () {
+    // Call the restartGame function when the button is clicked
+    restartGame();
+  });
+};
