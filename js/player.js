dad18c3440c9d45131a0704f40cb16b17c7d8c05
theflanders2 <kenneth.flanders.ii@gmail.com>
2023-11-13 15:56:13 +0100

    upate Player () class - comment out directionX, directionY and element from constructor - add comments to code - rename this.element to this.elementPlayerClimber

--- js/player.js
+++ js/player.js
@@ -1,14 +1,5 @@
 class Player {
-  constructor(
-    gameScreen,
-    left,
-    top,
-    width,
-    height,
-    directionX,
-    directionY,
-    element
-  ) {
+  constructor(gameScreen, left, top, width, height, /*directionX, directionY, element*/) {
     this.gameScreen = gameScreen;
     this.left = left;
     this.top = top;
@@ -16,45 +7,49 @@ class Player {
     this.height = height;
     this.directionX = 0;
     this.directionY = 0;
-    this.element = document.createElement("img");
+    this.elementPlayerClimber = document.createElement("img");
 
-    this.element.src = './images/playerClimber.png';
-    this.element.style.position = "absolute";
-    this.element.style.width = `${width}px`;
-    this.element.style.height = `${height}px`;
-    this.element.style.left = `${left}px`;
-    this.element.style.top = `${top}px`;
+    this.elementPlayerClimber.src = './images/playerClimber.png';
+    this.elementPlayerClimber.style.position = "absolute";
+    this.elementPlayerClimber.style.width = `${width}px`;
+    this.elementPlayerClimber.style.height = `${height}px`;
+    this.elementPlayerClimber.style.left = `${left}px`;
+    this.elementPlayerClimber.style.top = `${top}px`;
 
-    this.gameScreen.appendChild(this.element);
+    this.gameScreen.appendChild(this.elementPlayerClimber);
   }
 
-  move() {
+  move() { // update player position based on directionX and directionY
     this.left += this.directionX;
     this.top += this.directionY;
 
     if (this.left < 10) {
       this.left = 10;
     }
-    if (this.top < 10) {
+
+    if (this.top < 10) { // handles top side
       this.top = 10;
     }
-    if (this.left > this.gameScreen.offsetWidth - this.width - 10) {
+
+    if (this.left > this.gameScreen.offsetWidth - this.width - 10) { // handles right hand side
       this.left = this.gameScreen.offsetWidth - this.width - 10;
     }
-    if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
+
+    if (this.top > this.gameScreen.offsetHeight - this.height - 10) { // handles bottom side
       this.top = this.gameScreen.offsetHeight - this.height - 10;
     }
-    this.updatePosition();
+
+    this.updatePosition(); // update the player's position on screen
   }
 
   updatePosition() {
-    this.element.style.left = `${this.left}px`;
-    this.element.style.top = `${this.top}px`;
+    this.elementPlayerClimber.style.left = `${this.left}px`;
+    this.elementPlayerClimber.style.top = `${this.top}px`;
   }
 
   didCollide(obstacle) {
-    const playerRect = this.element.getBoundingClientRect();
-    const obstacleRect = obstacle.element.getBoundingClientRect();
+    const playerRect = this.elementPlayerClimber.getBoundingClientRect();
+    const obstacleRect = obstacle.elementPlayerClimber.getBoundingClientRect();
 
     if (
       playerRect.left < obstacleRect.right &&
@@ -63,7 +58,9 @@ class Player {
       playerRect.bottom > obstacleRect.top
     ) {
       return true;
-    } else {
+    } 
+    
+    else {
       return false;
     }
   }
